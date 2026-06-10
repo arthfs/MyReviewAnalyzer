19600033a50b8b164aed7929f7aaf4940cd13493
Marc-Arthur Saint louis <ms4050@scarletmail.rutgers.edu>
2026-06-10 15:45:55 -0400

    fixed config file

--- review_analyzer/next.config.ts
+++ review_analyzer/next.config.ts
@@ -2,11 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.12.60', 'localhost:3000', '127.0.0.1:3000', '127.0.0.1:8000'],
-  
-  // If you need standalone output for Docker
-  output: "standalone",
-  
-  // Other config options...
+  output: "standalone",  // This is fine for both Docker and Amplify
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
